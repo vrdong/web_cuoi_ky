5ef5752f818ce77410798697bbae47274a77808a
vrdong <caobadong19@gmail.com>
2018-06-22 10:58:59 +0700

    recaptcha

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+const request = require('request');
 
 var csrf = require('csurf');
 
@@ -25,7 +26,7 @@ router.get('/signup', function (req, res, next) {
   res.render('users/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 });
 
-router.post('/signup',passport.authenticate('local.signup',{
+router.post('/signup', passport.authenticate('local.signup', {
   successRedirect: '/',
   failureRedirect: '/users/signup',
   failureFlash: true
@@ -36,25 +37,54 @@ router.get('/signin', function (req, res, next) {
   res.render('users/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 })
 
-router.post('/signin',passport.authenticate('local.signin',{
-  successRedirect: '/',
-  failureRedirect: '/users/signin',
-  failureFlash: true
-}))
-
-module.exports = router;
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
+router.post('/signin', function (req, res, next) {
+  var captcha = req.body['g-recaptcha-response'];
+  if (
+    captcha === undefined ||
+    captcha === '' ||
+    captcha === null
+  ) {
+    return res.render('users/signin', { csrfToken: req.csrfToken(), msg: "Failed captcha verification"});
   }
-  res.redirect('/');
-}
+  // Secret Key
+  const secretKey = '	6Lc-BGAUAAAAAPOgorFcX1WFSPQWnnU9VpIMt3W_';
 
-function notLoggedIn(req, res, next) {
-  if (!req.isAuthenticated()) {
+  // Verify URL
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captcha}&remoteip=${req.connection.remoteAddress}`;
+
+  // Make Request To VerifyURL
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+    console.log(body);
+
+    // If Not Successful
+    if (body.success !== undefined && !body.success) {
+     return res.render('users/signin', { csrfToken: req.csrfToken(), msg: "Failed captcha verification"});
+    }
+
+    //If Successful
     return next();
+  })
+
+  }, passport.authenticate('local.signin', {
+    successRedirect: '/',
+    failureRedirect: '/users/signin',
+    failureFlash: true
+  }))
+
+  module.exports = router;
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
   }
-  res.redirect('/');
-}
+
+  function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  }
 
